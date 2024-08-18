29dfa205e2c8891bc97a5b1454c80f98f40bf8fe
Jefte Elioenai Zamarron Pule <Jefte_Zamarron23@unikino.edu.mx>
2024-08-18 14:19:45 -0700

    Intento de modificar la fecha

--- ApiVentas/Entities/Venta.cs
+++ ApiVentas/Entities/Venta.cs
@@ -5,8 +5,10 @@
         public int Id { get; set; }
         public int ClienteId { get; set; }
         public int ProductoId { get; set; }
-        public DateTime Fecha { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Today;
         public decimal Total { get; set; }
+
+
 
         // TODO: agregar mas campos.
     }
